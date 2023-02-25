09df91dad04b67721a162b3412eac87a65ccb1a8
Zach Anderson <zca721@outlook.com>
2023-02-24 18:13:58 -0800

    Created instance variables for the gridBagConstraints inside a new class GridConstraints.java. Then, implemented all the instance variables in TetrisGUI.java.

---
+++ src/frontend/GridConstraints.java
@@ -0,0 +1,101 @@
+package frontend;
+
+/**
+ * Holds instance variables for gridBagConstraints.
+ * @author Zachary C Anderson (zca721@uw.edu)
+ * @version 0.1
+ */
+public final class GridConstraints {
+
+    // Tetris panel instance variables.
+    // ----------------------------------------------------------------------------------------
+    /**
+     * Tetris panel weight x.
+     */
+    static final double TETRIS_PANEL_WEIGHT_X = 0.75;
+    /**
+     * Tetris panel weight y.
+     */
+    static final double TETRIS_PANEL_WEIGHT_Y = 1.0;
+    /**
+     * Tetris panel grid x.
+     */
+    static final int TETRIS_PANEL_GRID_X = 0;
+    /**
+     * Tetris panel grid y.
+     */
+    static final int TETRIS_PANEL_GRID_Y = 0;
+    /**
+     * Tetris panel grid height.
+     */
+    static final int TETRIS_PANEL_GRID_HEIGHT = 4;
+
+    // Tetromino panel instance variables.
+    // ----------------------------------------------------------------------------------------
+    /**
+     * Tetromino panel weight x.
+     */
+    static final double TETROMINO_PANEL_WEIGHT_X = 0.25;
+    /**
+     * Tetromino panel weight y.
+     */
+    static final double TETROMINO_PANEL_WEIGHT_Y = 0.3;
+    /**
+     * Tetromino panel grid x.
+     */
+    static final int TETROMINO_PANEL_GRID_X = 1;
+    /**
+     * Tetromino panel grid y.
+     */
+    static final int TETROMINO_PANEL_GRID_Y = 0;
+    /**
+     * Tetromino panel grid height.
+     */
+    static final int TETROMINO_PANEL_GRID_HEIGHT = 1;
+
+    // Stat panel instance variables.
+    // ----------------------------------------------------------------------------------------
+    /**
+     * Stat panel weight x.
+     */
+    static final double STAT_PANEL_WEIGHT_X = 0.25;
+    /**
+     * Stat panel weight y.
+     */
+    static final double STAT_PANEL_WEIGHT_Y = 0.6;
+    /**
+     * Stat panel grid x.
+     */
+    static final int STAT_PANEL_GRID_X = 1;
+    /**
+     * Stat panel grid y.
+     */
+    static final int STAT_PANEL_GRID_Y = 1;
+    /**
+     * Stat panel grid height.
+     */
+    static final int STAT_PANEL_GRID_HEIGHT = 3;
+
+    // Main panel instance variables.
+    // ----------------------------------------------------------------------------------------
+    /**
+     * Minimum screen size width.
+     */
+    static final int MINIMUM_SIZE_WIDTH = 360;
+    /**
+     * Minimum screen size height.
+     */
+    static final int MINIMUM_SIZE_HEIGHT = 480;
+
+    // Number values.
+    // ----------------------------------------------------------------------------------------
+    /**
+     * Value 3.
+     */
+    static final int VALUE_THREE = 3;
+    /**
+     * Value 2.
+     */
+    static final int VALUE_TWO = 2;
+
+}
--- src/frontend/TetrisGUI.java
+++ src/frontend/TetrisGUI.java
@@ -80,39 +80,43 @@ public class TetrisGUI extends JFrame {
         width to 0.
          */
 
-        c.weighty = 1.0;
-        c.weightx = 0.75;
-        c.gridx = 0;
-        c.gridy = 0;
-        c.gridheight = 4;
+        c.weightx = GridConstraints.TETRIS_PANEL_WEIGHT_X;
+        c.weighty = GridConstraints.TETRIS_PANEL_WEIGHT_Y;
+        c.gridx = GridConstraints.TETRIS_PANEL_GRID_X;
+        c.gridy = GridConstraints.TETRIS_PANEL_GRID_Y;
+        c.gridheight = GridConstraints.TETRIS_PANEL_GRID_HEIGHT;
         mainPanel.add(tetrisPanel, c);
 
-        c.weightx = 0.25;
-        c.weighty = 0.3;
-        c.gridx = 1;
-        c.gridy = 0;
-        c.gridheight = 1;
+        c.weightx = GridConstraints.TETROMINO_PANEL_WEIGHT_X;
+        c.weighty = GridConstraints.TETROMINO_PANEL_WEIGHT_Y;
+        c.gridx = GridConstraints.TETROMINO_PANEL_GRID_X;
+        c.gridy = GridConstraints.TETROMINO_PANEL_GRID_Y;
+        c.gridheight = GridConstraints.TETROMINO_PANEL_GRID_HEIGHT;
         mainPanel.add(tetrominoPanel, c);
         //TODO: Make this panel square, consider:
         //https://stackoverflow.com/questions/27544569/java-how-to-control-jpanel-aspect-ratio
 
-        c.weightx = 0.25;
-        c.weighty = 0.6;
-        c.gridx = 1;
-        c.gridy = 1;
-        c.gridheight = 3;
+        c.weightx = GridConstraints.STAT_PANEL_WEIGHT_X;
+        c.weighty = GridConstraints.STAT_PANEL_WEIGHT_Y;
+        c.gridx = GridConstraints.STAT_PANEL_GRID_X;
+        c.gridy = GridConstraints.STAT_PANEL_GRID_Y;
+        c.gridheight = GridConstraints.STAT_PANEL_GRID_HEIGHT;
         mainPanel.add(statPanel, c);
 
-        setPreferredSize(new Dimension((int) (SCREEN_SIZE.getWidth() / 3),
-                (int) (SCREEN_SIZE.getHeight() / 2)));
-        setMinimumSize(new Dimension(360, 480));
+        setPreferredSize(new Dimension(
+                (int) (SCREEN_SIZE.getWidth() / GridConstraints.VALUE_THREE),
+                (int) (SCREEN_SIZE.getHeight() / GridConstraints.VALUE_TWO)));
+        setMinimumSize(new Dimension(GridConstraints.MINIMUM_SIZE_WIDTH,
+                GridConstraints.MINIMUM_SIZE_HEIGHT));
         setResizable(true);
 
         add(mainPanel);
         pack();
 
-        setLocation(SCREEN_SIZE.width / 2 - getWidth() / 2,
-                SCREEN_SIZE.height / 2 - getHeight() / 2);
+        setLocation(SCREEN_SIZE.width / GridConstraints.VALUE_TWO
+                        - getWidth() / GridConstraints.VALUE_TWO,
+                SCREEN_SIZE.height / GridConstraints.VALUE_TWO
+                        - getHeight() / GridConstraints.VALUE_TWO);
 
 
     }
